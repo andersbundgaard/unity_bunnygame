f6b30b0fc5bab37136e9bd356eb753674156c378
andersbundgaard <andersbundgaard.dk@gmail.com>
2022-03-28 08:25:02 +0200

    idk

--- Assets/DeathZone.cs
+++ Assets/DeathZone.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.tag == "Player" || collision.tag =="Player_1"){
+        if (collision.tag == "Player" || collision.tag == "Player_1"){
             Destroy(collision.gameObject);
         }
     }
